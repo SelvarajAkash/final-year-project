267a524d2ec35c740157feb43822bd40d2c89d43
SELAVARAJ <selvaraj@gmail.com>
2023-06-07 08:48:02 +0100

    final year project

---
+++ Common/footer.php
@@ -0,0 +1,27 @@
+<footer>
+  <div class="footer-top">
+    <div class="container">
+        <div class="row">
+          <div class="col-md-4 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>UNDERGRADUATE PROGRAMS</h2>
+            <ul>
+              <li><a href="">B.Sc. Civil Engineering</a></li>
+              <li><a href="">B.Sc. Electrical Engineering</a></li>
+              <li><a href="">B.Sc. Mechanical Engineering</a></li>
+              <li><a href="">B.Sc. Architecture</a></li>
+              <li><a href="">B.Sc. Computer Science</a></li>
+              <li><a href="">Bachelors of Business Administration</a></li>
+            </ul>
+          </div>  
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>CONTACT DETAILS</h2>
+              <p>Address: 58,
+                  cuddalore main road - pakkam.</p>
+              <p>Telephones: +91 7200524286</p>
+              <p>E-mail: selvaraj1662003@gmail.com</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  <p class="footer-bottom-text">CopyRights © Alright Reserve 2023</p>
+</footer>
